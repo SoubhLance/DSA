8a291f5b062ea6fa8e3bebd6031ce5f9386a72dd
soubhlance <soubhiksadhu1981@gmail.com>
2025-08-17 23:54:10 +0530

    update

--- Arrays/array2.c
+++ Arrays/array2.c
@@ -1,5 +1,4 @@
-// Initializing the array with even numbers
-
+// Initializing the array with even numbers and odd numbers
 #include <stdio.h>
 
 int main()
@@ -9,14 +8,27 @@ int main()
     scanf("%d", &n);
 
     int a[n];
+    int b[n];
 
+    // Fill arrays with even and odd numbers
     for (int i = 0; i < n; i++) {
         a[i] = i * 2;
+        b[i] = i * 2 + 1;
     }
 
+    // Print even numbers
+    printf("The Even Number array is: ");
     for (int i = 0; i < n; i++) {
-        printf("%d ", a[i]);   
+        printf("%d ", a[i]);
     }
+    printf("\n"); // new line
+
+    // Print odd numbers
+    printf("The Odd Number array is: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", b[i]);
+    }
+    printf("\n");
 
     return 0;
 }
