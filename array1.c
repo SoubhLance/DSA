ab9375a6aebaeaf1e644db30856aff0cb4eede64
soubhlance <soubhiksadhu1981@gmail.com>
2025-08-17 23:28:50 +0530

    arrays revision

---
+++ array1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Give the number of values to be in the array: ");
+    scanf("%d", &n);
+
+    int a[n];  
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter value %d: ", i + 1);
+        scanf("%d", &a[i]);
+    }
+
+    printf("The values in the array are:\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
