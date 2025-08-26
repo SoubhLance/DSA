a054c4574f5f35c28ba4d9823fd62353c1b2de75
soubhlance <soubhiksadhu1981@gmail.com>
2025-08-26 19:05:36 +0530

    array8

---
+++ Arrays/ARRAY.md
@@ -0,0 +1,36 @@
+## array1.c
+-> Initializing an Array and then displaying it  
+-> WAP  to read and display n numbers using an array
+
+## array2.c
+-> Initializing the array with even numbers and odd numbers
+
+## array3,c
+-> WAP to read and display n random integers in an array
+
+## array4.c
+-> WAP to print the position of the smallest of n numbers in an array
+
+## array5.c
+-> WAP to interchange the smallest and the largest number
+
+## array6.c
+-> WAP to find the second largest number using array of n numbers 
+
+## array7.c
+```
+    Exercise 5.5
+    Data[] is an array that is declared as int Data[20] and contains the following values:
+    Data[] = {12, 23, 34, 45, 56, 67, 78, 89, 90, 100}
+    (a) Calculate the length of the array.
+    (b) Find the upper bound and lower bound.
+    (c) Show the memory representation of the array.
+    (d) If a new data element with value 75 has to be inserted, find its position.
+    (e) Insert the new data element and then show the memory representation of the array.
+```
+
+## array8.c
+-> WAP to insert a number at a given location
+
+## array9.c
+->
--- Arrays/array8.c
+++ Arrays/array8.c
@@ -1,35 +1,56 @@
-//WAP to insert a number at a given location 
+//WAP to insert a number at a given location
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    int arr[10];
-    for(int i=0;i<10;i++)
-    {
-        arr[i]=rand()%100;
+int main() {
+    int n;
+    printf("Enter the number of elements in an array");
+    scanf("%d",&n);
+    int arr[n];
+
+    for(int i = 0; i < n ; i++){
+        scanf("%d", &arr[i]);
     }
-    printf("Original Array\n");
-    for(int i=0;i<10;i++)
-    {
-        printf("%d\t",arr[i]);
-    }
-    int pos,val;
-    printf("\nEnter the position the element to be inserted: ");
-    scanf("%d",&pos);
-    printf("\nEnter the value of the element to be inserted: ");
-    scanf("%d",&val);
+    
 
-    for(int i = 10;i>pos;i--)
-    {
-        arr[i]=arr[i-1];
-    }
-    arr[pos]=val;
+    // // Fill array with random numbers
+    // for (int i = 0; i < n; i++) {
+    //     arr[i] = rand() % 100;
+    // }
 
-    for(int i=0;i<10;i++)
-    {
-        printf("%d\t",arr[i]);
+    printf("Original Array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t", arr[i]);
     }
 
+    int pos, val;
+    printf("\nEnter the position of the new element to insert: ");
+    scanf("%d", &pos);
+    printf("\nEnter the value of the new element to insert: ");
+    scanf("%d", &val);
+
+    //checking for the invalid position
+    if (pos < 0 || pos > n) {
+        printf("Invalid position!\n");
+        return 0;
+    }
+
+    // Shift elements to the right
+    for (int i = n; i > pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+   
+    arr[pos] = val; // insert new value
+     n++;            // increase size
+
+    printf("\nArray after insertion:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t", arr[i]);
+    }
+
+    int new_size = sizeof(arr)/sizeof(arr[0]);
+    printf("\nNew size of array is : %d", new_size);
+
+    return 0;
 }
